6498e52508d70c14c67491de7cee9cd3ee23eb39
yemma203 <106160039+yemma203@users.noreply.github.com>
2023-12-19 12:01:04 +0100

    Local changes

--- back/index.js
+++ back/index.js
@@ -26,87 +26,15 @@ app.use(express.json());
 
 // GET
 
-import React, { useState, useEffect } from "react";
-
-export default function Home() {
-  const [cards, setCards] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [cardsPerPage, setCardsPerPage] = useState(8);
-  const [sortParam, setSortParam] = useState(''); // Default sort by name
-
-  const getCards = async () => {
+app.get('/cards', async (req, res) => {
     try {
-      const response = await fetch(`http://localhost:8000/cards?sort=${sortParam}`);
-      const jsonData = await response.json();
-
-      setCards(jsonData);
+        const conn = await pool.getConnection();
+        const rows = await conn.query("SELECT * FROM cards");
+        res.status(200).json(rows);
     } catch (err) {
-      console.error(err.message);
+        console.log(err);
     }
-  };
-
-  useEffect(() => {
-    getCards();
-  }, [sortParam, currentPage]);
-
-  // Check if cards is an array before slicing
-  const currentCards = Array.isArray(cards) ? cards.slice(
-    (currentPage - 1) * cardsPerPage,
-    currentPage * cardsPerPage
-  ) : [];
-
-  return (
-    <div>
-      <div>
-        {/* Dropdown for sorting */}
-        <label>Trier par :</label>
-        <select
-          value={sortParam}
-          onChange={(e) => setSortParam(e.target.value)}
-        >
-          <option value="name">Nom</option>
-          <option value="rarity">Rareté</option>
-          <option value="price">Prix</option>
-        </select>
-      </div>
-      <div className="cards">
-        {currentCards.map((card) => (
-          <div className="card" key={card.id}>
-            <div className="imgCard">
-              <img src={card.img_url} alt="img of card" />
-            </div>
-            <p>
-              Prix Ebay :{" "}
-              {card.ebay_price === 0.0 ? "Prix inconnu" : card.ebay_price} $
-            </p>
-            <p>{card.rarity}</p>
-          </div>
-        ))}
-      </div>
-      <div className="pagination">
-        <button
-          onClick={() => {
-            if (currentPage > 1) {
-              setCurrentPage(currentPage - 1);
-            }
-          }}
-        >
-          Précédent
-        </button>
-        <button
-          onClick={() => {
-            if (currentPage < Math.ceil(cards.length / cardsPerPage)) {
-              setCurrentPage(currentPage + 1);
-            }
-          }}
-        >
-          Suivant
-        </button>
-      </div>
-    </div>
-  );
-}
-
+});
 
 // POST
 
